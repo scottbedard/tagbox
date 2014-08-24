acf4ca11c393367146180ddc23eccd6fe84c4564
Scott Bedard <scottbedard90@gmail.com>
2014-08-24 04:36:24 -0400

    Tagbox

---
+++ README.md
@@ -0,0 +1,7 @@
+## Tagbox 1.0
+
+For examples and documentation visit [http://scottbedard.net/tagbox](http://scottbedard.net/tagbox)
+
+### License
+
+Tagbox is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ demo.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="tagbox-blue.css">
+
+</head>
+
+<body>
+
+    <input id="demo" data-tags="one|two|three|four" placeholder="Enter tags..." />
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+    <script src="tagbox.js"></script>
+
+    <script>
+
+        $('#demo').tagbox();
+
+    </script>
+</body>
+</html>
